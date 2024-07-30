b7ced5badfe9d9ad60f8981e7f7737f5a7f7f071
jqtmviyu <jqtmviyu@gmail.com>
2024-07-31 03:42:11 +0800

    fix: :bug: update variable name

--- update_transmission_blacklist.sh
+++ update_transmission_blacklist.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # 设置变量
-HOST="127.0.0.1"
-PORT="9091"
-USERNAME="用户名"
-PASSWORD="密码"
+host="127.0.0.1"
+port="9091"
+username="用户名"
+password="密码"
 
 # 更新 Transmission 黑名单
 echo "更新 Transmission 黑名单..."
-transmission-remote $HOST:$PORT --auth $USERNAME:$PASSWORD --blocklist-update
+transmission-remote $host:$port --auth $username:$password --blocklist-update
 
 if [ $? -eq 0 ]; then
     echo "Transmission 黑名单更新成功,重启transmission"
